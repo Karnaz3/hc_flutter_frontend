068d5779e5afeca9cfea53404a36dec118dfccc5
Sourav <souravtowork@gmail.com>
2024-08-20 18:04:41 +0545

    added features in most section

--- lib/network.dart
+++ lib/network.dart
@@ -5,6 +5,9 @@ class ApiService {
 
   ApiService()
       : dio = Dio(BaseOptions(
+        /**
+         * add your backend port and IP address here
+         */
           baseUrl: 'http://192.168.1.188:3000', // Set your base URL here
           connectTimeout: const Duration(milliseconds: 5000), // 5 seconds
           receiveTimeout: const Duration(milliseconds: 3000), // 3 seconds
